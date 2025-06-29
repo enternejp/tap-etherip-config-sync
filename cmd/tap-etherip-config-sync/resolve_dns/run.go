785f9df4d50a81d17a1493b22afdb83fcac5ddba
sobadon <sbdn@chasoba.net>
2025-06-29 09:26:40 +0000

    refactor

--- cmd/tap-etherip-config-sync/resolve_dns/run.go
+++ cmd/tap-etherip-config-sync/resolve_dns/run.go
@@ -43,46 +43,50 @@ func run(cmd *cobra.Command, args []string) error {
 func resolveConfig(in *config.ConfWithDNS, l *zap.Logger) (*config.Conf, error) {
 	var out config.Conf
 	ctx := context.Background()
-	for _, tw := range in.Tunnels {
+	for _, inTunl := range in.Tunnels {
 		var localIP, remoteIP string
-		if tw.LocalFQDN != "" {
-			ips, err := dns.ResolveAAAA(ctx, tw.LocalFQDN)
+
+		if inTunl.LocalFQDN == "" && inTunl.LocalIPAddr == "" {
+			l.Error("LocalFQDN and LocalIPAddr are both empty", zap.String("tunnel", inTunl.Name))
+			continue
+		}
+		if inTunl.LocalFQDN != "" {
+			ips, err := dns.ResolveAAAA(ctx, inTunl.LocalFQDN)
 			if err != nil {
-				l.Warn("LocalFQDN resolve failed", zap.String("fqdn", tw.LocalFQDN), zap.Error(err))
+				l.Warn("LocalFQDN resolve failed", zap.String("fqdn", inTunl.LocalFQDN), zap.Error(err))
 			} else {
 				if len(ips) > 1 {
-					l.Warn("LocalFQDN multiple AAAA records. Use first IPv6 address.", zap.String("fqdn", tw.LocalFQDN), zap.Strings("dns_answers", ips))
+					l.Warn("LocalFQDN multiple AAAA records. Use first IPv6 address.", zap.String("fqdn", inTunl.LocalFQDN), zap.Strings("dns_answers", ips))
 				}
 				localIP = ips[0]
 			}
 		}
-		if localIP == "" {
-			localIP = tw.LocalIPAddr
+		// prefer *IPAddr field
+		if inTunl.LocalIPAddr != "" {
+			localIP = inTunl.LocalIPAddr
 		}
-		if localIP == "" {
-			l.Error("LocalFQDN and LocalIPAddr are both empty", zap.String("tunnel", tw.Name))
+
+		if inTunl.RemoteFQDN == "" && inTunl.RemoteIPAddr == "" {
+			l.Error("RemoteFQDN and RemoteIPAddr are both empty", zap.String("tunnel", inTunl.Name))
 			continue
 		}
-		if tw.RemoteFQDN != "" {
-			ips, err := dns.ResolveAAAA(ctx, tw.RemoteFQDN)
+		if inTunl.RemoteFQDN != "" {
+			ips, err := dns.ResolveAAAA(ctx, inTunl.RemoteFQDN)
 			if err != nil {
-				l.Warn("RemoteFQDN resolve failed", zap.String("fqdn", tw.RemoteFQDN), zap.Error(err))
+				l.Warn("RemoteFQDN resolve failed", zap.String("fqdn", inTunl.RemoteFQDN), zap.Error(err))
 			} else {
 				if len(ips) > 1 {
-					l.Warn("RemoteFQDN multiple AAAA records. Use first IPv6 address.", zap.String("fqdn", tw.RemoteFQDN), zap.Strings("dns_answers", ips))
+					l.Warn("RemoteFQDN multiple AAAA records. Use first IPv6 address.", zap.String("fqdn", inTunl.RemoteFQDN), zap.Strings("dns_answers", ips))
 				}
 				remoteIP = ips[0]
 			}
 		}
-		if remoteIP == "" {
-			remoteIP = tw.RemoteIPAddr
+		if inTunl.RemoteIPAddr != "" {
+			remoteIP = inTunl.RemoteIPAddr
 		}
-		if remoteIP == "" {
-			l.Error("RemoteFQDN and RemoteIPAddr are both empty", zap.String("tunnel", tw.Name))
-			continue
-		}
+
 		out.Tunnels = append(out.Tunnels, config.Tunnel{
-			Name:         tw.Name,
+			Name:         inTunl.Name,
 			LocalIPAddr:  localIP,
 			RemoteIPAddr: remoteIP,
 		})
